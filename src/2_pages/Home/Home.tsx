16bb89bebba7afd758861b01f1883dfcc2d351f7
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-07-14 18:35:40 +0300

    add home page

--- src/2_pages/Home/Home.tsx
+++ src/2_pages/Home/Home.tsx
@@ -1,12 +1,30 @@
-import { FC } from "react"
+import { FC, useCallback } from "react"
 
 import { BeverageLimitList } from "../../3_widgets/BeverageLimitList/ui"
+import { Toolbar } from "../../3_widgets/Toolbar/ui"
+import { useInfiniteBeverages } from "../../5_entities/Beverage/lib/hooks"
+import { useScrollIntoView } from "../../6_shared/lib/hooks"
 import styles from "./Home.module.scss"
 
 export const Home: FC = () => {
+  const { fetchNextPage, isLoading } = useInfiniteBeverages()
+
+  const { scrollIntoView, ref } = useScrollIntoView<HTMLDivElement>()
+
+  const nextPage = useCallback(() => {
+    fetchNextPage()
+
+    scrollIntoView()
+  }, [fetchNextPage, scrollIntoView])
+
   return (
-    <div className={styles.home}>
-      <BeverageLimitList />
+    <div ref={ref} className={styles.home}>
+      <header className={styles.toolbar}>
+        <Toolbar fetchNextPage={nextPage} />
+      </header>
+      <article className={styles.list}>
+        <BeverageLimitList fetchNextPage={nextPage} isLoading={isLoading} />
+      </article>
     </div>
   )
 }
