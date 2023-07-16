c2af8582e1e6fa2839b52c70a9a05c77b8604f05
Rudenko Vladimir <72340706+RudVlad473@users.noreply.github.com>
2023-07-16 10:09:43 +0300

    implement routing

--- src/1_app/router/router.tsx
+++ src/1_app/router/router.tsx
@@ -2,14 +2,16 @@ import { createBrowserRouter } from "react-router-dom"
 
 import { BeverageViewPage } from "../../2_pages/BeverageViewPage"
 import { Home } from "../../2_pages/Home"
+import { TRoute } from "../../6_shared/lib/types"
+import { dynamicRoute } from "../../6_shared/lib/utils"
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: TRoute.HOME,
     element: <Home />,
   },
   {
-    path: "beverage/:beverageId",
+    path: dynamicRoute(TRoute.BEVERAGE_BY_ID),
     element: <BeverageViewPage />,
   },
 ])
---
+++ src/6_shared/lib/utils/dynamic-route.ts
@@ -0,0 +1,3 @@
+export function dynamicRoute(route: string): string {
+  return "/:" + route
+}
